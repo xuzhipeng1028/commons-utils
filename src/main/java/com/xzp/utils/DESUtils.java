99befdda1ef63e17b36f3445f72ff9a506cf0af7
xuzhipeng <xuzhipeng@nucarf.com>
2022-02-18 15:05:00 +0800

    DES加解密工具类

--- src/main/java/com/xzp/utils/DESUtils.java
+++ src/main/java/com/xzp/utils/DESUtils.java
@@ -28,11 +28,6 @@ public final class DESUtils {
      */
     private static final String CIPHER_ALGORITHM = "DES/CBC/PKCS5Padding";
 
-    /**
-     * 偏移变量，固定占8位字节
-     */
-    private final static String IV_PARAMETER = "12345678";
-
     private DESUtils(){
         throw new UnsupportedOperationException(CommonConstants.INSTANTIATE_UTILITY_CLASS_EXCEPTION);
     }
@@ -40,38 +35,40 @@ public final class DESUtils {
     /**
      * 加密
      * @param password 加密密码，长度不能够小于8位
+     * @param iv 偏移变量，固定占8位字节
      * @param content 待加密字符串
      * @return 加密后内容
      * @throws Exception 异常
      */
-    public static String encrypt(String password,String content) throws Exception {
-        return handle(password, content.getBytes(StandardCharsets.UTF_8), Cipher.ENCRYPT_MODE);
+    public static String encrypt(String password,String iv,String content) throws Exception {
+        return handle(password, iv, content.getBytes(StandardCharsets.UTF_8), Cipher.ENCRYPT_MODE);
     }
 
     /**
      * 解密
      * @param password 解密密码，长度不能够小于8位
+     * @param iv 偏移变量，固定占8位字节
      * @param content 待解密字符串
      * @return 解密后内容
      * @throws Exception 异常
      */
-    public static String decrypt(String password,String content) throws Exception {
-        return handle(password, Base64.getDecoder().decode(content.getBytes(StandardCharsets.UTF_8)), Cipher.DECRYPT_MODE);
+    public static String decrypt(String password,String iv,String content) throws Exception {
+        return handle(password, iv, Base64.getDecoder().decode(content.getBytes(StandardCharsets.UTF_8)), Cipher.DECRYPT_MODE);
     }
 
     /**
      * 处理加解密
      * @param password 密码，长度不能够小于8位
+     * @param iv 偏移变量，固定占8位字节
      * @param bytes 待加解密字节数组
      * @param mode 模式
      * @return 加解密后内容
      * @throws Exception 异常
      */
-    private static String handle(String password,byte[] bytes,int mode) throws Exception {
+    private static String handle(String password,String iv,byte[] bytes,int mode) throws Exception {
         Key secretKey = generateKey(password);
         Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM);
-        IvParameterSpec iv = new IvParameterSpec(IV_PARAMETER.getBytes(StandardCharsets.UTF_8));
-        cipher.init(mode, secretKey, iv);
+        cipher.init(mode, secretKey, new IvParameterSpec(iv.getBytes(StandardCharsets.UTF_8)));
         switch (mode){
             case Cipher.ENCRYPT_MODE:
                 return new String(Base64.getEncoder().encode(cipher.doFinal(bytes)));
