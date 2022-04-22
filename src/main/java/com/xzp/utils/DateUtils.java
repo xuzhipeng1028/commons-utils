8fab62a6501bf53499cc748808921024d91ee72d
xuzhipeng <xuzhipeng@nucarf.com>
2022-04-22 10:37:23 +0800

    日期工具类

--- src/main/java/com/xzp/utils/DateUtils.java
+++ src/main/java/com/xzp/utils/DateUtils.java
@@ -1,7 +1,10 @@
 package com.xzp.utils;
 
 import com.xzp.common.constants.CommonConstants;
+import javafx.util.Pair;
 import org.apache.commons.lang3.time.FastDateFormat;
+
+import java.util.*;
 
 /**
  * 日期工具类
@@ -22,6 +25,116 @@ public final class DateUtils {
 
     private DateUtils(){
         throw new UnsupportedOperationException(CommonConstants.INSTANTIATE_UTILITY_CLASS_EXCEPTION);
+    }
+
+    /**
+     * 获取今天的开始时间
+     * @return {@link Date}
+     */
+    public static Date getTodayStartTime(){
+        Calendar cal = new GregorianCalendar();
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+        return cal.getTime();
+    }
+
+    /**
+     * 获取今天的结束时间
+     * @return {@link Date}
+     */
+    public static Date getTodayEndTime(){
+        Calendar cal = new GregorianCalendar();
+        cal.set(Calendar.HOUR_OF_DAY, 23);
+        cal.set(Calendar.MINUTE, 59);
+        cal.set(Calendar.SECOND, 59);
+        return cal.getTime();
+    }
+
+    /**
+     * 获取某天的开始时间
+     * @return {@link Date}
+     */
+
+    /**
+     * 获取某天的开始时间
+     * @param date 时间
+     * @return {@link Date}
+     */
+    public static Date getSomeDayStartTime(Date date){
+        Calendar cal = new GregorianCalendar();
+        cal.setTime(date);
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+        return cal.getTime();
+    }
+
+    /**
+     * 获取某天的结束时间
+     * @param date 时间
+     * @return {@link Date}
+     */
+    public static Date getSomeDayEndTime(Date date){
+        Calendar cal = new GregorianCalendar();
+        cal.setTime(date);
+        cal.set(Calendar.HOUR_OF_DAY, 23);
+        cal.set(Calendar.MINUTE, 59);
+        cal.set(Calendar.SECOND, 59);
+        return cal.getTime();
+    }
+
+    /**
+     * 获取某天明天的开始时间
+     * @param date 时间
+     * @return {@link Date}
+     */
+    public static Date getSomeDayTomorrowStartTime(Date date){
+        Calendar cal = new GregorianCalendar();
+        cal.setTime(date);
+        cal.add(Calendar.DAY_OF_MONTH, 1);
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+        return cal.getTime();
+    }
+
+    /**
+     * 获取某天明天的结束时间
+     * @param date 时间
+     * @return {@link Date}
+     */
+    public static Date getSomeDayTomorrowEndTime(Date date){
+        Calendar cal = new GregorianCalendar();
+        cal.setTime(date);
+        cal.add(Calendar.DAY_OF_MONTH, 1);
+        cal.set(Calendar.HOUR_OF_DAY, 23);
+        cal.set(Calendar.MINUTE, 59);
+        cal.set(Calendar.SECOND, 59);
+        return cal.getTime();
+    }
+
+    /**
+     * 获取时间段内每一天的开始和结束时间
+     * @param startDate 开始时间
+     * @param endDate 结束时间
+     * @return {@link List < Pair <Date,Date>>}
+     */
+    public static List<Pair<Date,Date>> getEveryDayStartAndEnd(Date startDate,Date endDate){
+        if (Objects.isNull(startDate) || Objects.isNull(endDate) || endDate.before(startDate)) {
+            throw new IllegalArgumentException("参数有误");
+        }
+        List<Pair<Date,Date>> list = new ArrayList<>();
+        Date beginDate = getSomeDayStartTime(startDate);
+        Date finalDate = getSomeDayEndTime(endDate);
+        while (finalDate.after(beginDate)){
+            list.add(new Pair<>(beginDate,getSomeDayEndTime(beginDate)));
+            beginDate = getSomeDayTomorrowStartTime(beginDate);
+        }
+        return list;
     }
 
 }
